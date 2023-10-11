66abbdbed4a2479039fe253767b7884976565263
Emotional-emptiness <moon.linexx@gmail.com>
2023-10-11 10:56:47 -0700

    first commit from linux xd

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        int i = 0;
+        String kali = "kalilinux";
+        while (i<kali.length()){
+            System.out.println("hello from linux");
+            i++;
+        }
+    }
+}
