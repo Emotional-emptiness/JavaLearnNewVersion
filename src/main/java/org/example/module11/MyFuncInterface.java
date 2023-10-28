3118b0cfe5412af395647ef91b7d0a3a1a99a3f6
Emotional-emptiness <moon.linexx@gmail.com>
2023-10-28 14:32:22 +0300

    ...

--- src/main/java/org/example/module11/MyFuncInterface.java
+++ src/main/java/org/example/module11/MyFuncInterface.java
@@ -1,5 +1,5 @@
 package org.example.module11;
-
+@FunctionalInterface
 public interface MyFuncInterface {
     int multiplication(int a, int b);
 }
--- src/main/java/org/example/module11/MyLambdaTest.java
+++ src/main/java/org/example/module11/MyLambdaTest.java
@@ -3,8 +3,12 @@ public class MyLambdaTest {
     public static void main(String[] args) {
         MyMultiplication myMultiplication = new MyMultiplication();
         int mult = myMultiplication.multiplication(5,5, (a, b) -> a*b );
-        int mult2 = myMultiplication.multiplication(4,6, (a, b) -> a*b*a);
+        int mult1 = myMultiplication.multiplication(4,6, (a, b) -> a*b*a);
+        int mult2 = myMultiplication.multiplication(2, 3, (a, b) -> {
+            return (a + b)*(a + b);
+        });
         System.out.println("mult = " + mult);
-        System.out.println("mult = " + mult2);
+        System.out.println("mult1 = " + mult1);
+        System.out.println("mult2 = " + mult2);
     }
 }
