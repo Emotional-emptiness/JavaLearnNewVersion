c8b24f7d365bd44780390f198a1572e4430d7675
Emotional-emptiness <moon.linexx@gmail.com>
2023-11-29 22:01:43 +0300

    ...

--- src/main/java/org/example/module11/FunctionalInterfaceDemo.java
+++ src/main/java/org/example/module11/FunctionalInterfaceDemo.java
@@ -1,10 +1,9 @@
 package org.example.module11;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class FunctionalInterfaceDemo {
     private static List<String> emails = Arrays.asList("oleksander.yanov@google.com"
@@ -12,10 +11,21 @@ public class FunctionalInterfaceDemo {
             , "anton@gmail.com", "serhii@google.com");
 
     public static void main(String[] args) {
-
+        // func example
         final Map<String, Integer> result = calculateEmailLetters(emails, email -> email.length());
         System.out.println("result = " + result);
 
+        // consumer example
+        Consumer<Map<String, Integer>> consumer = map -> {
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                System.out.println("Email : " + entry.getKey() + " hava " + entry.getValue() + " letters");
+            }
+        };
+        consumer.accept(result);
+
+        // supplier example
+        Supplier<Integer> randomDigit = () -> new Random().nextInt(100) * 50;
+        System.out.println(randomDigit.get());
     }
     private static Map<String,Integer> calculateEmailLetters(List<String> emails, Function<String,Integer> function) {
         Map<String, Integer> result = new HashMap<>();
