030de0b80bb1106aed1f4e17138c4e18eb5bed40
Emotional-emptiness <moon.linexx@gmail.com>
2023-12-27 21:26:16 +0300

    ...

--- src/main/java/org/example/module11/StreamDemo.java
+++ src/main/java/org/example/module11/StreamDemo.java
@@ -1,15 +1,53 @@
 package org.example.module11;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class StreamDemo {
-    private static List<Employee> employees = new ArrayList<>();
+    private static final List<Employee> employees = new ArrayList<>(Arrays.asList(
+            new Employee(5,"Bohdan", UserStatus.ACTIVE),
+        new Employee(4, "Andrew", UserStatus.ACTIVE),
+        new Employee(3, "Taras", UserStatus.NOT_ACTIVE),
+        new Employee(2, "",UserStatus.NOT_ACTIVE),
+        new Employee(13, "Andrew",UserStatus.NOT_ACTIVE),
+        new Employee(1, "Serg",UserStatus.ACTIVE),
+        new Employee(8, "James",UserStatus.NOT_ACTIVE),
+        new Employee(9, "Jon",UserStatus.ACTIVE),
+        new Employee(16, "Misha",UserStatus.ACTIVE),
+        new Employee(22, "Lexa",UserStatus.ACTIVE),
+        new Employee(212, "Drake",UserStatus.NOT_ACTIVE)
+            ));
 
-    {
-        employees.add(new Employee(2,"Bohdan", UserStatus.ACTIVE));
-        employees.add(new Employee(3, "Andrew", UserStatus.ACTIVE));
-        employees.add(new Employee(4, "Taras", UserStatus.NOT_ACTIVE));
-        employees.add(new Employee(5, "Andrew",UserStatus.NOT_ACTIVE));
+    public static void main(String[] args) {
+        // filter by active status
+        List<Employee> activeEmployee = new ArrayList<>();
+        for (Employee employee : employees) {
+            if (employee.getStatus() == UserStatus.ACTIVE) {
+                activeEmployee.add(employee);
+            }
+        }
+        // sort
+        activeEmployee.sort(new Comparator<Employee>() {
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                return Integer.compare(o1.getId(), o2.getId());
+            }
+        });
+
+        // map names
+        List<String> employeeNames = new ArrayList<>();
+        for(Employee employee : activeEmployee) {
+            employeeNames.add(employee.getName());
+        }
+
+        List<String> employeeWithStream = employees.stream()
+                .filter(employee -> employee.getStatus().equals(UserStatus.ACTIVE))
+                .sorted(Comparator.comparingInt(Employee::getId))
+                .map(Employee::getName)
+                .toList();
+        System.out.println(employeeNames);
+        System.out.println(employeeWithStream);
     }
 }
--- src/main/java/org/example/module11/UserStatus.java
+++ src/main/java/org/example/module11/UserStatus.java
@@ -1,5 +1,16 @@
 package org.example.module11;
 
 public class UserStatus {
-    public static UserStatus ACTIVE, NOT_ACTIVE;
+    public static final UserStatus ACTIVE = new UserStatus("ACTIVE");
+    public static final UserStatus NOT_ACTIVE = new UserStatus("NOT_ACTIVE");
+
+    private final String status;
+
+    private UserStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
