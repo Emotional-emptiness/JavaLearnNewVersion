9241fb215e7a962ea0ddf442ea0ddf8a42fd5795
Emotional-emptiness <moon.linexx@gmail.com>
2024-03-12 16:31:48 +0300

    ...

--- src/main/java/org/example/module12/ThreadDemo.java
+++ src/main/java/org/example/module12/ThreadDemo.java
@@ -1,5 +1,6 @@
 package org.example.module12;
 
+
 public class ThreadDemo  {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Main поток стартовал " + Thread.currentThread().getName());
@@ -12,5 +13,11 @@ public class ThreadDemo  {
         Thread thread1 = new ChildThread();
         System.out.println("Thread.currentThread().getPriority() = " + Thread.currentThread().getPriority());
         thread1.start();
+
+
+        new Thread(() -> System.out.println("Hello Thread Lambda")).start();
+        Thread thread2 = new Thread(() -> System.out.println(Thread.currentThread().getPriority() + " -> Priority lambda"));
+        thread2.setPriority(1);
+        thread2.start();
     }
 }
